37371afe55cc26757baaf5172e52db7b5a1bd651
South Manse <southmanse@Souths-MBP.attlocal.net>
2025-02-19 13:24:50 -0500

    good stopping point. lint isn't whining any more. but this has a huge performance issue because of call depth. so maybe it's time to refactor.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, MouseEvent, useEffect, useState } from "react"
+import { ChangeEvent, Fragment, MouseEvent, useEffect, useState, useCallback } from "react"
 
 enum LocalStorageKeys {
   ENTRY_LIST = "ENTRY_LIST"
@@ -9,30 +9,41 @@ const App = () => {
     startTime: number
     elapsedTime: number
     description: string
+    isSuddenlyAwakeEntry: boolean
   }
 
+  const [isFirstAwakeEntry, setIsFirstAwakeEntry] = useState(false)
   const [isRequestDeleteConfirm, setIsRequestDeleteConfirm] = useState(false)
   const [isRequestDelete, setIsRequestDelete] = useState(false)
   const [now, setNow] = useState(Date.now())
-  const [suddenlyIWasAwake, setSuddenlyIWasAwake] = useState(false)
+  const [suddenlyIWasAwake, setSuddenlyIWasAwake] = useState(0)
   const [entryList, setEntryList] = useState<Entry[]>([])
   const [startTime, setStartTime] = useState(0);
   const [description, setDescription] = useState("")
 
-  function saveLocalStorageEntryList() {
+  function beginAndEndAwakeEntry() {
+    if (isFirstAwakeEntry) {
+      endEntry()
+      setIsFirstAwakeEntry(false)
+    }
+  }
+
+
+
+  const saveLocalStorageEntryList = useCallback((entries?: Entry[]) => {
     try {
-      localStorage.setItem(LocalStorageKeys.ENTRY_LIST, JSON.stringify(entryList))
+      localStorage.setItem(LocalStorageKeys.ENTRY_LIST, JSON.stringify(entries ? entries : entryList))
     } catch (e) {
       console.log("Unable to save entry list to local storage.")
       console.log(e)
       localStorage.setItem(LocalStorageKeys.ENTRY_LIST, JSON.stringify([]))
     }
-  }
+  }, [entryList])
 
   function loadLocalStorageEntryList() {
     try {
-      const localStorageEntryListString = localStorage.getItem(LocalStorageKeys.ENTRY_LIST)
-      const localStorageEntryExists = !!localStorageEntryListString;
+      const localStorageEntryListString = localStorage.getItem(LocalStorageKeys.ENTRY_LIST) ?? '';
+      const localStorageEntryExists = Boolean(localStorageEntryListString);
       const localStorageEntryListObject = localStorageEntryExists ? JSON.parse(localStorageEntryListString) : null
       if (Array.isArray(localStorageEntryListObject)) {
         setEntryList(localStorageEntryListObject)
@@ -63,25 +74,30 @@ const App = () => {
     setDescription("")
   }
 
-  const endEntry = (event?: MouseEvent<HTMLButtonElement>) => {
+  const endEntry = useCallback((event?: MouseEvent<HTMLButtonElement>) => {
     event?.preventDefault()
-
     const elapsedTime = Date.now() - startTime
+    const isSuddenlyAwakeEntry = Math.abs(elapsedTime) < 100;
     const updatedEntries = [...entryList, {
       startTime,
       elapsedTime,
-      description
+      description,
+      isSuddenlyAwakeEntry
     }]
     setEntryList(updatedEntries)
+    saveLocalStorageEntryList(updatedEntries)
     beginEntry()
-  }
+  }, [description, entryList, saveLocalStorageEntryList, startTime])
 
   const awaken = () => {
-    setSuddenlyIWasAwake(true)
+    setSuddenlyIWasAwake(Date.now())
     setInterval(() => {
       setNow(Date.now())
     }, 100)
-    beginEntry()
+
+    setStartTime(Date.now(),)
+    setDescription("Suddenly I was awake!")
+    setIsFirstAwakeEntry(true)
   }
 
   function handleChangeDescription(changedDescriptionEvent: ChangeEvent<HTMLInputElement>) {
@@ -101,42 +117,40 @@ const App = () => {
     setIsRequestDeleteConfirm(false)
   }
 
-  useEffect(loadLocalStorageEntryList, [])
+  useEffect(beginAndEndAwakeEntry, [isFirstAwakeEntry, endEntry])
+  useEffect(loadLocalStorageEntryList, [endEntry])
 
   return (
     <>
+      <h1>Score: {entryList.length}</h1>
       <div>
         {!suddenlyIWasAwake && <button style={{ fontSize: 27 }} onClick={awaken}>Suddenly, I was awake.</button>}
-        {suddenlyIWasAwake && (
-          <>
-            <div>
-              <button onClick={saveLocalStorageEntryList}>Save Entry List to Local Storage</button>
-            </div>
-            <div>
-              <button onClick={loadLocalStorageEntryList}>Load Entry List Local Storage</button>
-            </div>
-            <div>
-              <button onClick={removeLocalStorageEntryList}>Remove Entry List from Local Storage</button>
-            </div>
-          </>
-        )}
       </div>
 
       <form onSubmit={handleSubmit}>
         <ul>
           {entryList.map((entry, i) => (
-            <li key={i}>
-              [{entry.elapsedTime}]: {entry.description}
-            </li>
+            <Fragment key={i}>
+              {(entry.isSuddenlyAwakeEntry ?
+                <li>
+                  [{entry.startTime}]: {entry.description}
+                </li> :
+                <li>
+                  [{entry.elapsedTime}]: {entry.description}
+                </li>
+              )}
+            </Fragment>
           ))}
           <li>
-            [{suddenlyIWasAwake ? (now - startTime) : 0}]:
-            <input value={description} onChange={handleChangeDescription} />
-            {suddenlyIWasAwake && <button onClick={endEntry}>LOG</button>}
+            [{suddenlyIWasAwake !== 0 ? (now - startTime) : "---"}]:
+            {suddenlyIWasAwake !== 0 ? <input autoFocus value={description} onChange={handleChangeDescription} /> : "---"}
+            {suddenlyIWasAwake !== 0 ? <button onClick={endEntry}>LOG</button> : "---"}
           </li>
         </ul>
       </form>
-
+      <div>
+        <button onClick={removeLocalStorageEntryList}>CLEAR ALL ENTRIES</button>
+      </div>
       {isRequestDelete && !isRequestDeleteConfirm && (
         <>
           <div>Are you sure you want to remove this data?</div>
